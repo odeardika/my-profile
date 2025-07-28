720524686165c27107e9339f13be87770a9ae905
Ode Ardika <dirfanardika@gmail.com>
2025-07-28 19:24:48 +0800

    feat: create login page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -12,15 +12,15 @@ export default function RootLayout() {
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
 
-  if (!loaded) {
-    // Async font loading only occurs in development.
-    return null;
-  }
+  // Remove the font loading check for debugging
+  // if (!loaded) {
+  //   return null;
+  // }
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
       <StatusBar style="auto" />
